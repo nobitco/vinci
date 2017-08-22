684155ae84f04a84f108308877ecea28c48a49a3
jesmarquez <jesmqz@gmail.com>
2017-08-22 11:38:27 -0500

    Instala material-ui
     Se implementa material-ui y se establece user-agent.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,24 @@
-export default () => (
-  <div>Vinci iniciando!</div>
-)
+import '../components/tap_events'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import FlatButton from 'material-ui/FlatButton'
+
+export default class extends React.Component {
+  static async getInitialProps({req}) {
+    return {
+      userAgent: req ? req.headers['user-agent'] : navigator.userAgent,
+    }
+  }
+  render() {
+    const muiTheme = getMuiTheme({
+      userAgent: this.props.userAgent,
+    });
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <FlatButton label="Iniciando vinci!" />
+      </MuiThemeProvider>
+    )
+  }
+}
